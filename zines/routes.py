0143fd8545b2a96a6ba48cd43ae8d0edcd1e7bb8
M. Ethan Black <markblack96@gmail.com>
2019-10-24 07:52:50 -0500

    Intermediary push

--- zines/routes.py
+++ zines/routes.py
@@ -1,19 +1,41 @@
 from flask import render_template, request
-from . import app
+from . import app, db
 from . import models
 from .forms import CreatePost
+from lxml.html.clean import Cleaner
+from bs4 import BeautifulSoup
 
+
+@app.route('/')
+@app.route('/index')
+def index():
+    posts = models.Post.query.all()
+    return render_template("index.html", posts=posts)
 @app.route('/post/')
 @app.route('/post/<post_id>')
-def index(post_id=None):
+def post(post_id=None):
     post = models.Post.query.filter_by(post_id=post_id).first()
-    # post = {'title': 'The Great Corn Heist',
-    #        'content': content}
-    return render_template('post.html', post=post)
+    soup = BeautifulSoup(post.content, 'html.parser')
+    sections = soup.find_all(['h1', 'h2', 'h3'])
+    sections = [section.get_text() for section in sections]
+    print(sections)
+    return render_template('post.html', post=post, sections=sections)
 
 @app.route('/write', methods=["GET", "POST"])
 def write():
     form = CreatePost()
     if request.method == "POST":
-        print(request.form.get('delta'))
+        cleaner = Cleaner(allow_tags=['p', 'h1', 'h2', 'h3'],
+                          remove_unknown_tags=False)
+        post = cleaner.clean_html(request.form.get('delta'))
+        soup = BeautifulSoup(post, 'html.parser')
+        title = soup.find_all('h1')[0].string # todo: check if exists first
+        for h1 in soup("h1"): # remove all h1 tags
+            h1.decompose()
+
+        post=soup.prettify()
+        submission = models.Post(title=title, author="meb", content=post)
+        db.session.add(submission)
+        db.session.commit()
+        print(f"Title: {title}" )
     return render_template('write.html', form=form)

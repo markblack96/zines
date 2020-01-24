da3c2d4b6187e18856e26bb36fc1b794a8061730
eblack <markblack96@gmail.com>
2020-01-24 08:46:03 -0600

    Cosmetic changes

--- zines/routes.py
+++ zines/routes.py
@@ -11,10 +11,11 @@ from bs4 import BeautifulSoup
 @app.route('/index')
 def index():
     blog_title = app.config['TITLE']
+    blog_description = app.config['DESCRIPTION']
     posts = models.Post.query.all()
     previews = {post.post_id:BeautifulSoup(post.content).p.text for post in posts}
     #{post.post_id:BeautifulSoup(post.content).get_text(" ", strip=True)[:500] + "..." for post in posts}
-    return render_template("index.html", posts=posts, blog_title=blog_title, previews=previews)
+    return render_template("index.html", posts=posts, blog_title=blog_title, blog_description=blog_description, previews=previews)
 
 @app.route('/post/')
 @app.route('/post/<post_id>')

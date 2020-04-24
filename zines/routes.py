ee25d2e539f020c501108bb0a6fd09a24badc896
eblack <markblack96@gmail.com>
2020-04-24 16:30:15 -0500

    Moving toward image support

--- zines/routes.py
+++ zines/routes.py
@@ -1,10 +1,12 @@
-from flask import render_template, request, redirect, flash, url_for
+import os
+from flask import render_template, request, redirect, flash, url_for, send_from_directory
 from flask_login import login_required, current_user, login_user, logout_user
 from . import app, db
 from . import models
 from .forms import CreatePost, LoginForm
 from lxml.html.clean import Cleaner
 from bs4 import BeautifulSoup
+from werkzeug.utils import secure_filename
 
 
 @app.route('/')
@@ -95,3 +97,47 @@ def delete(post_id):
 def admin():
     posts = models.Post.query.all()
     return render_template('admin.html', posts=posts)
+
+
+# Functions below are functional with config set up but we need to make them upload filename (timestamp)
+# to database and associate with blog post id. Then we will need to make templates display the image if
+# one exists or a default image otherwise.
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/upload/image', methods=['GET', 'POST'])
+def upload_file():
+    print(request)
+    if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # if user does not select file, browser also submit an empty part without filename
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                flash("Something should have happened")
+                return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.route('/edit/image/<post_id>')
+def edit_image(post_id):
+    pass
